383a36af0e39b271fa58a5ab10614d326d522302
zhangjie <82853247+ShinyNito@users.noreply.github.com>
2023-05-29 10:56:38 +0800

    Create modify_chnroutes.py

---
+++ modify_chnroutes.py
@@ -0,0 +1,22 @@
+import requests
+import re
+
+# Get chnroutes.txt from upstream
+url = 'https://raw.githubusercontent.com/misakaio/chnroutes2/master/chnroutes.txt'
+response = requests.get(url)
+data = response.text
+
+# Process each line
+lines = data.split('\n')
+for i in range(len(lines)):
+    line = lines[i]
+    if re.match(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/\d{1,2}$', line):
+        # Insert IP-CIDR and ,no-resolve
+        lines[i] = 'IP-CIDR ' + line + ',no-resolve'
+    else:
+        # Output the line as is
+        lines[i] = line
+
+# Write modified data to file
+with open('chnroutes.txt', 'w') as f:
+    f.write('\n'.join(lines))
